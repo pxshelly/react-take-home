96191785ccef25298f0abdcb9c8b8a632af85253
Shelly <pxshelly@gmail.com>
2019-07-01 22:25:04 -0700

    Initial render
      - render campaign name
      - render icon image
      - render pay per install

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Campaign from './Campaign';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campaigns: []
+  }
+}
+
+  componentDidMount() {
+    axios.get('https://www.plugco.in/public/take_home_sample_feed')
+      .then(response => {
+        console.log(response.data.campaigns);
+        this.setState({campaigns: response.data.campaigns})
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  
+  render() {
+    const campaign = this.state.campaigns.map((campaign, i) => 
+      <Campaign campaign = {campaign} key = {i}/>
+    )
+    return (
+      <div className="App">
+        {campaign}
+      </div>
+    );
+  }
 }
 
 export default App;
