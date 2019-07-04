5e72810c78ade6126a5766c8db98e09b42e90055
Shelly <pxshelly@gmail.com>
2019-07-03 18:39:09 -0700

    Implement error handling for failed API requests

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Campaign from './Campaign';
 import axios from 'axios';
+import Error from './Error.jsx';
 import './App.css';
 
 
@@ -8,28 +9,42 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      campaigns: []
+      campaigns: [],
+      hasError: false
+    }
+    this.refresh = this.refresh.bind(this);
   }
-}
+
+  refresh() {
+    axios.get('https://www.plugco.in/public/take_home_sample_feed')
+    .then(response => {
+      console.log(response.data.campaigns);
+      this.setState({
+        campaigns: response.data.campaigns,
+        hasError: false
+      })
+    })
+    .catch(error => {
+      this.setState({hasError: true});
+    })
+  }
 
   componentDidMount() {
-    axios.get('https://www.plugco.in/public/take_home_sample_feed')
-      .then(response => {
-        console.log(response.data.campaigns);
-        this.setState({campaigns: response.data.campaigns})
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    }
+    this.refresh();
+  }
   
   render() {
-    const campaign = this.state.campaigns.map((campaign, i) => 
-      <Campaign campaign = {campaign} key = {i}/>
-    )
+    let body = '';
+    if (this.state.hasError) {
+      body = <Error />
+    } else {
+      body = this.state.campaigns.map((campaign, i) => 
+        <Campaign campaign={campaign} key={i}/>
+      )
+    }
     return (
       <div className="App">
-        {campaign}
+        {body}
       </div>
     );
   }
